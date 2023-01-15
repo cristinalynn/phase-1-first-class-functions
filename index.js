86b94ab70e0a8a390e2b601ca69586739d78e442
Cristina Lynn <cristina@cristinalynn.net>
2023-01-15 15:25:40 -0800

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(cb) {
+        return cb();
+}
+function returnsANamedFunction() {
+    return function namedFunction() {
+
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        
+    }
+}
